06c2dd83fbe273e856f97ed6d38eeaf3a6e316a8
mdave16 <mdave3927@gmail.com>
2018-03-27 00:12:43 +0100

    complete basic idea MVC

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,6 +2,18 @@ class IdeasController < ApplicationController
   def new
   end
 
-  def create
+  def show
+    @idea = Idea.find(params[:id])
   end
+
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.save
+    redirect_to @idea
+  end
+
+  private
+    def idea_params
+      params.require( :idea).permit( :title, :text)
+    end
 end
