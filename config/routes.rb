b258c64a19efb722a2f34c8ad37d0fde9e4e1cc2
mdave16 <mdave3927@gmail.com>
2019-01-14 22:42:04 +0000

    allow votes for ideas

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   delete 'signout', to: 'sessions#destroy', as: 'signout'
 
-  resources :ideas
+  resources :ideas do
+  	member do
+			put 'like', to: 'ideas#upvote'
+			put 'dislike', to: 'ideas#downvote' 
+		end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
